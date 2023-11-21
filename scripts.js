c9db4f6bcff8052c3a8b8847e56a97dcfa9cf5c7
Mahdi <bitsgenix@gmail.com>
2023-11-21 17:22:40 +0330

    add enmie charecters

--- scripts.js
+++ scripts.js
@@ -1,25 +1,83 @@
 // Box Game
-let boxGame ;
-let boxWidth = 960
-let boxHeight = 530
+let boxGame;
+let boxWidth = 960;
+let boxHeight = 530;
 let context;
 
 //Dinosaur
-let dinoWidth = 88
-let dinoHeight = 94
-let dinoX = 50
-let dionY = boxHeight - dinoHeight
-let Img;
+let dinoWidth = 88;
+let dinoHeight = 94;
+let dinoX = 50;
+let dionY = boxHeight - dinoHeight;
+let dinoImg;
 
-let dino= {
-    x:dinoX,
-    y:dionY,
-    width:dinoWidth,
-    height:dinoHeight
+let dino = {
+  x: dinoX,
+  y: dionY,
+  width: dinoWidth,
+  height: dinoHeight,
+};
+// enemi
+let enmeiArray = [];
+let enmei1Width = 34;
+let enmei2Width = 69;
+let enemi3Width = 102;
+
+let enemiHeight = 70;
+let enemiX = 700;
+let enemiY = boxHeight - enemiHeight;
+
+let enemi1Img;
+let enemi2Img;
+let enemi3Img;
+window.addEventListener("load", () => {
+  boxGame = document.getElementById("BoxGame");
+  boxGame.height = boxHeight;
+  boxGame.width = boxWidth;
+  context = boxGame.getContext("2d");
+  //   context.fillStyle = "green";
+  //   context.fillRect(dino.x, dino.y, dino.width, dino.height);
+  dinoImg = new Image();
+  dinoImg.src = "./img/DinoDefault.png";
+  dinoImg.onload = function () {
+    context.drawImage(dinoImg, dino.x, dino.y, dino.width, dino.height);
+  };
+
+  enemi1Img = new Image();
+  enemi1Img.src = "./DinoDefault2.png";
+  enemi2Img = new Image();
+  enemi2Img.src = "./DinoDefault2.png";
+  enemi3Img = new Image();
+  enemi3Img.src = "./DinoDefault2.png";
+
+  requestAnimationFrame(update);
+  setInterval(placeEnmei, 1000);
+});
+function update() {
+  requestAnimationFrame(update);
+  context.drawImage(dinoImg, dino.x, dino.y, dino.width, dino.height);
 }
-window.addEventListener('load',()=>{
-    boxGame = document.getElementById('BoxGame')
-    boxGame.height = boxHeight
-    boxGame.width = boxWidth
-    context = boxGame.getContext('2d'); 
-})
+function placeEnmei() {
+  let enemi = {
+    img: null,
+    x: enemiX,
+    y: enemiY,
+    width: null,
+    height: enemiHeight,
+  };
+  let placeEnmeiChange = Math.random();
+  if (placeEnmeiChange > 0.9) { // 10%
+    enemi.img = enemi3Img;
+    enemi.width = enemi3Width;
+    enmeiArray.push(enemi);
+  } else if (placeEnmeiChange > 0.7) { //30%
+    enemi.img = enemi2Img;
+    enemi.width = enmei2Width;
+    enmeiArray.push(enemi);
+}else if(placeEnmeiChange > .50) {// 50%
+    enemi.img = enemi1Img;
+    enemi.width = enmei1Width;
+    enmeiArray.push(enemi);
+}
+
+}
