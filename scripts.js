62cb01032a961c6138f22862ae097fe04881024f
mahdi <mahdimamashli1383@gmail.com>
2023-11-21 17:03:35 +0330

    start dino js

---
+++ scripts.js
@@ -0,0 +1,22 @@
+// Box Game
+let boxGame ;
+let boxWidth = 960
+let boxHeight = 530
+let context;
+
+//Dinosaur
+let dinoWidth = 88
+let dinoHeight = 94
+let dinoX = 50
+let dionY = boxHeight - dinoHeight
+let Img;
+
+let dino= {
+    x:dinoX,
+    y:dionY,
+    width:dinoWidth,
+    height:dinoHeight
+}
+window.addEventListener('load',()=>{
+    boxGame = document.getElementById('BoxGame')
+})
