d131022230943a9cc1c86662592e15f1e5786dd7
mahdi <mahdimamashli1383@gmail.com>
2023-11-22 13:51:53 +0330

    add and change img

--- scripts.js
+++ scripts.js
@@ -17,22 +17,22 @@ let dino = {
   width: dinoWidth,
   height: dinoHeight,
 };
-// enemi physics
-let enmeiArray = [];
-let enmei1Width = 70;
-let enmei2Width = 100;
-let enemi3Width = 152;
+// enemy physics
+let enemyArray = [];
+let enemy1Width = 70;
+let enemy2Width = 100;
+let enemy3Width = 152;
 
-let enemiHeight = 120;
-let enemiX = 950;
-let enemiY = boxHeight - enemiHeight + 15;
+let enemyHeight = 120;
+let enemyX = 950;
+let enemyY = boxHeight - enemyHeight + 15;
 
-let enemi1Img;
-let enemi2Img;
-let enemi3Img;
+let enemy1Img;
+let enemy2Img;
+let enemy3Img;
 
 // physics
-let velocityX = -8; // enime Moving left speed
+let velocityX = -8; // enemy Moving left speed
 let velocityY = 0;
 let gravity = 0.4;
 let GameOver = false;
@@ -42,23 +42,21 @@ window.addEventListener("load", () => {
   boxGame.height = boxHeight;
   boxGame.width = boxWidth;
   context = boxGame.getContext("2d");
-  //   context.fillStyle = "green";
-  //   context.fillRect(dino.x, dino.y, dino.width, dino.height);
   dinoImg = new Image();
   dinoImg.src = "./img/DinoDefault.png";
   dinoImg.onload = function () {
     context.drawImage(dinoImg, dino.x, dino.y, dino.width, dino.height);
   };
 
-  enemi1Img = new Image();
-  enemi1Img.src = "./img/DinpDefault2.png";
-  enemi2Img = new Image();
-  enemi2Img.src = "./img/DinpDefault2.png";
-  enemi3Img = new Image();
-  enemi3Img.src = "./img/DinpDefault2.png";
+  enemy1Img = new Image();
+  enemy1Img.src = "./img/dinoEnemy.png";
+  enemy2Img = new Image();
+  enemy2Img.src = "./img/dinoEnemy2.png";
+  enemy3Img = new Image();
+  enemy3Img.src = "./img/dinoEnemy3.png";
 
   requestAnimationFrame(update);
-  setInterval(placeEnmei, 1000);
+  setInterval(placeEnemy, 1000);
   document.addEventListener("keydown", moveDino);
 });
 function update() {
@@ -71,12 +69,12 @@ function update() {
   velocityY += gravity;
   dino.y = Math.min(dino.y + velocityY, dionY);
   context.drawImage(dinoImg, dino.x, dino.y, dino.width, dino.height);
-  //enemi
-  for (let i = 0; i < enmeiArray.length; i++) {
-    let enemi = enmeiArray[i];
-    enemi.x += velocityX;
-    context.drawImage(enemi.img, enemi.x, enemi.y, enemi.width, enemi.height);
-    if (detectCollision(dino, enemi)) {
+  //enemy
+  for (let i = 0; i < enemyArray.length; i++) {
+    let enemy = enemyArray[i];
+    enemy.x += velocityX;
+    context.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
+    if (detectCollision(dino, enemy)) {
       GameOver = true;
       dinoImg.src = "./img/DinoDefault.png";
       dinoImg.onload = function () {
@@ -100,36 +98,36 @@ function moveDino(e) {
     velocityY = -12;
   }
 }
-function placeEnmei() {
+function placeEnemy() {
   if (GameOver) {
     return;
   }
-  let enemi = {
+  let enemy = {
     img: null,
-    x: enemiX,
-    y: enemiY,
+    x: enemyX,
+    y: enemyY,
     width: null,
-    height: enemiHeight,
+    height: enemyHeight,
   };
-  let placeEnmeiChange = Math.random();
-  if (placeEnmeiChange > 0.9) {
+  let placeEnemyChange = Math.random();
+  if (placeEnemyChange > 0.9) {
     // 10%
-    enemi.img = enemi3Img;
-    enemi.width = enemi3Width;
-    enmeiArray.push(enemi);
-  } else if (placeEnmeiChange > 0.7) {
+    enemy.img = enemy3Img;
+    enemy.width = enemy3Width;
+    enemyArray.push(enemy);
+  } else if (placeEnemyChange > 0.7) {
     //30%
-    enemi.img = enemi2Img;
-    enemi.width = enmei2Width;
-    enmeiArray.push(enemi);
-  } else if (placeEnmeiChange > 0.5) {
+    enemy.img = enemy2Img;
+    enemy.width = enemy2Width;
+    enemyArray.push(enemy);
+  } else if (placeEnemyChange > 0.5) {
     // 50%
-    enemi.img = enemi1Img;
-    enemi.width = enmei1Width;
-    enmeiArray.push(enemi);
+    enemy.img = enemy1Img;
+    enemy.width = enemy1Width;
+    enemyArray.push(enemy);
   }
-  if (enmeiArray.length > 5) {
-    enmeiArray.shift();
+  if (enemyArray.length > 5) {
+    enemyArray.shift();
   }
 }
 
